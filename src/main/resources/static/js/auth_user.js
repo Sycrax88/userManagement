71d9bf1271554c70fc1e0c31d24e0e6d9791a1ac
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2023-05-27 18:45:50 -0500

    Se hizo la funcionalidad de cerrar sesión

--- src/main/resources/static/js/auth_user.js
+++ src/main/resources/static/js/auth_user.js
@@ -5,3 +5,7 @@ $(document).ready(function() {
 function updateUserName(){
     document.getElementById('txtUserName').outerHTML = localStorage.firstName + " ";
 }
+
+function logout(){
+    localStorage.clear();
+}
